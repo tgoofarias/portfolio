2a479cfd381c6f5d9adfb9e2a0314407dd22d8f4
tgoofarias <tfb.snf21@uea.edu.br>
2024-04-17 15:52:41 -0400

    feat: adding dinamic portfolio

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Portfolio
+
 
 def home(request):
-    return render(request, 'portfolio/portfolio.html')
+    if not Portfolio.objects.filter(is_active=True).exists():
+        return render(request, 'portfolio/error.html')
+
+    portfolio = Portfolio.objects.filter(is_active=True).order_by('-updated').first()
+    social_medias = portfolio.social_medias.all()
+    contacts = portfolio.contacts.all()
+
+    context = {
+        'portfolio': portfolio,
+        'social_medias': social_medias,
+        'contacts': contacts
+    }
+
+    return render(request, 'portfolio/portfolio.html', context)
